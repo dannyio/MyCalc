246be1f0998a01d53d32e12b3d51fba9553f5795
Dannyio <itsdannyio01@gmail.com>
2017-12-07 16:43:17 -0500

    Laid out Ui. Provided tags for numbers. Ability to display userInput.

--- MyCalc/DesignClasses.swift
+++ MyCalc/DesignClasses.swift
@@ -13,7 +13,18 @@ class calcButton: UIButton {
         self.backgroundColor = .white
         self.layer.cornerRadius = 8
         self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowRadius = 5
+        self.layer.shadowRadius = 2
+        self.layer.shadowOpacity = 0.1
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+    }
+}
+
+class calcView: UIView {
+    override func didMoveToWindow() {
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 8
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowRadius = 2
         self.layer.shadowOpacity = 0.1
         self.layer.shadowOffset = CGSize(width: 0, height: 0)
     }
